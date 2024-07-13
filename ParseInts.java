e30209f8eee1e9928108c81d8056ac86a5a820bc
Ifiyemi Owi <ifiyemi.owi@keyin.com>
2024-07-13 04:37:36 -0230

    modified2

--- ParseInts.java
+++ ParseInts.java
@@ -2,10 +2,9 @@
 // ParseInts.java
 //
 // Reads a line of text and prints the integers in the line.  
-//Uses a second Scanner to take the input line one token at a time and parses an integer from each token as it is extracted. 
-//Sums the integers. 
-//Prints the sum. 
-
+// Uses a second Scanner to take the input line one token at a time and parses an integer from each token as it is extracted. 
+// Sums the integers. 
+// Prints the sum.          
 // ****************************************************************
 
 import java.util.Scanner;
@@ -18,39 +17,78 @@ public class ParseInts
 	Scanner scan = new Scanner(System.in);
 	String line;
 
-    // user input prompt
 	System.out.println("Enter a line of text");
 	Scanner scanLine = new Scanner(scan.nextLine());
 
-	// while (scanLine.hasNext())
-	//     {		
-	// 	val = Integer.parseInt(scanLine.next());
-	// 	sum += val;
-	//     }
+    try {
+        while (scanLine.hasNext()) {
+            val = Integer.parseInt(scanLine.next());
+            sum += val;
+        }
+    } catch (NumberFormatException e) {
+        System.out.println("Not an integer: " + scanLine.next());
+    }
+	
+	System.out.println("The sum of the integers on this line is " + sum);
+    }
 
-    // try statement encompassing entire while loop
-    // try {
-    //     //each token process in the line
-    //     while (scanLine.hasNext()) {
-    //         val = Integer.parseInt(scanLine.next());
-    //         sum += val;
-    //     }
-    // } catch (NumberFormatException e) {
-    //     System.out.println("Not an integer: " + scanLine.next());
-    // }
+}
 
+
+/* // ****************************************************************
+// ParseInts.java
+//
+// Reads a line of text and prints the integers in the line.  
+//Uses a second Scanner to take the input line one token at a time and parses an integer from each token as it is extracted. 
+//Sums the integers. 
+//Prints the sum. 
+
+// ****************************************************************
+
+import java.util.Scanner;
+
+public class ParseInts {
+    public static void main(String[] args) {
+	int val, sum=0;
+	Scanner scan = new Scanner(System.in) {
+    String line;
+
+    // user input prompt
+    System.out.println("Enter a line of text");
+    Scanner scanLine = new Scanner(scan.nextLine()) {
+            
     // Program modified moving try catch inside loop
     while (scanLine.hasNext()) {
         try {
             val = Integer.parseInt(scanLine.next());
             sum += val;
-        } catch (NumberFormatException e) {
+            } 
+        }catch (NumberFormatException e) {
             System.out.println("Not an integer: " + scanLine.next());
+            }
+
+
+        // while (scanLine.hasNext())
+        //     {		
+        // 	val = Integer.parseInt(scanLine.next());
+        // 	sum += val;
+        //     }
+
+        // try statement encompassing entire while loop
+        // try {
+        //     //each token process in the line
+        //     while (scanLine.hasNext()) {
+        //         val = Integer.parseInt(scanLine.next());
+        //         sum += val;
+        //     }
+        // } catch (NumberFormatException e) {
+        //     System.out.println("Not an integer: " + scanLine.next());
+        // }
+
         }
-    }
-    // prints sum of integers
+	// prints sum of integers
 	System.out.println("The sum of the integers on this line is " + sum);
     }
+*/
 
-}
 
