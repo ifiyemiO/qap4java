d7fa4c33cd0c6ea16ac01c2e5a723dd53c4db21f
Ifiyemi Owi <ifiyemi.owi@keyin.com>
2024-07-13 05:00:04 -0230

    modified ParseInt

--- ParseInts.java
+++ ParseInts.java
@@ -11,8 +11,7 @@ import java.util.Scanner;
 
 public class ParseInts
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
 	int val, sum=0;
 	Scanner scan = new Scanner(System.in);
 	String line;
@@ -20,15 +19,14 @@ public class ParseInts
 	System.out.println("Enter a line of text");
 	Scanner scanLine = new Scanner(scan.nextLine());
 
-    try {
         while (scanLine.hasNext()) {
-            val = Integer.parseInt(scanLine.next());
-            sum += val;
+            try {
+                val = Integer.parseInt(scanLine.next());
+                sum += val;
+        } catch (NumberFormatException e) {
+        //System.out.println("Not an integer: " + scanLine.next());
         }
-    } catch (NumberFormatException e) {
-        System.out.println("Not an integer: " + scanLine.next());
     }
-	
 	System.out.println("The sum of the integers on this line is " + sum);
     }
 
